ac4640d3e2ac1f7526eb4b24417d88fa4415ce19
Rodrigo de Almeida Francisco <rodrigoalmeidafrancisco@gmail.com>
2022-04-09 20:41:11 -0300

    Trabalhando com Authenticação.

---
+++ Front-end/BlazorApp/Data/DefaultContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlazorApp.Data
+{
+    public class DefaultContext : DbContext
+    {
+        public DefaultContext() : base()
+        {
+
+        }
+
+        public DefaultContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
